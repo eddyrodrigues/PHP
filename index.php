b18abae21b12946c3cc7ccd94abd3c65e533185e
Eduardo Oliveira Rodrigues <edu.ol.ro@gmail.com>
2016-08-10 11:53:39 -0300

    Atualizada
    Foram adicionados alguns arquivos para ser usado como base no meu
    servidor de Tibia.
    Logo o site acabou indo para este lado, mas nada de especial e exclusivo
    para o Tibia em sí, apenas algumas modificações para apresentar
    informações do servidor e quem sabe em breve algo mais. Thanks!

--- index.php
+++ index.php
@@ -5,48 +5,51 @@
 </head>
 <body>
 <?php
-$id = $_REQUEST['id']; // Aqui esta difinido o request //
-if($id != "") {	
-	switch($id) { 
-		// aqui a case//
-			
-		case "index":
-		case "noticias": 
-			$pagina = "noticieis.php"; 
-			break; 
-		case "about":
-			$pagina = "about.php"; 
-			break; 
-		case "contato":
-			$pagina = "contato.php"; 
-			break; 
-		// Agora definiremos o default, que será a pagina que será aberta quando não houver um valor para o $id 
-		default: 
-			$pagina = "erro.php"; 
-			break; 
-		case "admin":
-			$pagina = "administrador/admin.php";
-			break;	
-		case "add_noticia":
-			$pagina = "add_notice.php";
-			break;
-			
-	} 
+if(isset($_REQUEST['id'])){
+		$id = $_REQUEST['id']; // Aqui esta difinido o request //
+		if($id != "") {
+			switch($id) {
+				// aqui a case//
+				case "serverinfo":
+					$pagina = "serverinfo.php";
+					break;
+				case "index":
+				case "noticias":
+					$pagina = "noticieis.php";
+					break;
+				case "about":
+					$pagina = "about.php";
+					break;
+				case "contato":
+					$pagina = "contato.php";
+					break;
+				// Agora definiremos o default, que será a pagina que será aberta quando não houver um valor para o $id
+				default:
+					$pagina = "erro.php";
+					break;
+				case "admin":
+					$pagina = "administrador/admin.php";
+					break;
+				case "rank":
+					$pagina = "rank.php";
+					break;
+
+			}
+		}
+}else {
+	header("Location: index.php?id=noticias");
 }
-else{ 
-	$pagina = "noticieis.php";
-	}
 ?>
 
 <div class="header">
-<span class="topo"><img src="flor_topo.png" alt="Flower"></span>
+<span class="topo"></span>
   <p>sistema central</p>
 </div>
 <div class="menu">
 	<ul>
-		<li> <a href="index.html">home</a> </li>
+		<li> <a href="index.html">Home</a> </li>
 		<li> <a href="#">about</a> </li>
-		<li><a href="#">portifolio</a></li>
+		<li><a href="#">ServerInfo</a></li>
 	</ul>
 </div>
 <div class="corpo_de_fundo">
@@ -56,30 +59,29 @@ else{
 				<th><h1>Menu Principal</h1></th>
 			<tr>
 			</tr>
-				<td>	
+				<td>
 					<ul>
 						<li> <a href="index.php">home</a> </li>
-						<li> <a href="?id=add_noticia">add noticia</a> </li>
-						<li> <a href="?id=index">portifolio</a></li>
-					</ul>					
+						<li> <a href="?id=rank">Rank</a> </li>
+						<li> <a href="?id=serverinfo">ServerInfo</a></li>
+					</ul>
 				</td>
 			</tr>
 			<tr>
-				<th style = "border:1px solid;"><h1>aaaaaaaasadokasd</h1></th>
-			</tr>			
+				<th style = "border:1px solid;"><h1></h1></th>
+			</tr>
 		</table>
 	</div>
 	<div class="barra_direita">
 	<?php
 		if( (isset($pagina)) and (file_exists($pagina)) ) {  //a aqui se a pagina nao existir //
-			include($pagina); 
-		} else { 
-			echo "<tr><th><p><center>Página solicitada não existe!</center><p></th></tr>"; 
-		} 
+			include($pagina);
+		} else {
+			echo "<tr><th><p><center>Página solicitada não existe!</center><p></th></tr>";
+		}
 	?>
 	</div>
 </div>
 <footer class="menu"><a href="?id=admin">administrador</a></footer>
 </body>
 </html>
-
---
+++ serverinfo.php
@@ -0,0 +1,14 @@
+<?php
+include_once("confs_server.cfg")
+?>
+
+<table>
+<tr><th><h1>Informações do servidor</h1></th></tr>
+<tr><th>
+<h2>Experience Rate : x<?= $exprate ?></h2>
+<h2>Skills Rate: x<?= $skillsrate ?></h2>
+<h2>Loot Rate: x<?= $lootrate ?></h2>
+<h2>Magic Rate x<?= $magicrate ?></h2>
+<h2>Spawn Rate x<?= $spawnrate ?></h2>
+</th></tr>
+</table>
